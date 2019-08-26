7e4178c5b5e57e5b82db903cde808b104eaa47ee
phyzerbert <xian1017@outlook.com>
2019-08-26 15:45:16 +0800

    sidebar

--- public/master/js/custom.js
+++ public/master/js/custom.js
@@ -4,33 +4,40 @@
 	function isMobile() { return ('ontouchstart' in document.documentElement); }
 
 	// console.log(isMobile())
-
-	if(sessionStorage.getItem('sidebar') === null){
+	if(isMobile()){
 		$('.sidebar').removeClass('toggled');
-		if(isMobile()){
-			$(".main").width("100%")
-		}else{
-			$(".main").width("calc(100% - 255px)")
-		}
+		$(".main").width("100%")
 	}else{
-		$('.sidebar').addClass('toggled');
-		if(isMobile()){
-			$(".main").width("calc(100% - 255px)")
-		}else{
-			$(".main").width("100%")
-		}
+		$('.sidebar').removeClass('toggled');
+		$(".main").width("calc(100% - 255px)")
 	}
+
+	// if(sessionStorage.getItem('sidebar') === null){
+	// 	$('.sidebar').removeClass('toggled');
+	// 	if(isMobile()){
+	// 		$(".main").width("100%")
+	// 	}else{
+	// 		$(".main").width("calc(100% - 255px)")
+	// 	}
+	// }else{
+	// 	$('.sidebar').addClass('toggled');
+	// 	if(isMobile()){
+	// 		$(".main").width("calc(100% - 255px)")
+	// 	}else{
+	// 		$(".main").width("100%")
+	// 	}
+	// }
 	
 	$('.sidebar-toggle').click(function(event) {
 		if($(".sidebar").hasClass("toggled")){
-			sessionStorage.clear();	
+			// sessionStorage.clear();	
 			if(isMobile()){
 				$(".main").width("100%")
 			}else{
 				$(".main").width("calc(100% - 255px)")
 			}
 		}else{
-			sessionStorage.setItem('sidebar', 'expanded');
+			// sessionStorage.setItem('sidebar', 'expanded');
 			if(isMobile()){
 				$(".main").width("calc(100% - 255px)")
 			}else{
